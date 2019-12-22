1874403f72e86945c53dbb67422960a182807185
Sean Bernath <sbernath@gmail.com>
2019-12-22 17:48:20 -0500

    Updates

---
+++ precache-manifest.7f178d59e9aac5d42d95d1d365112605.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfc9023ad5dbc58f164ce630a7ba0de4",
+    "url": "/DnDSite/index.html"
+  },
+  {
+    "revision": "2de7bf3bc797400988e7",
+    "url": "/DnDSite/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "6bd90fd1edac1468f123",
+    "url": "/DnDSite/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2de7bf3bc797400988e7",
+    "url": "/DnDSite/static/js/2.ba6f5b50.chunk.js"
+  },
+  {
+    "revision": "6bd90fd1edac1468f123",
+    "url": "/DnDSite/static/js/main.e4f24fff.chunk.js"
+  },
+  {
+    "revision": "f679b0bc2c2813dfdd59",
+    "url": "/DnDSite/static/js/runtime-main.718ef516.js"
+  },
+  {
+    "revision": "22e9aec0d368b4437ea5021d478a9f2d",
+    "url": "/DnDSite/static/media/UserImage.22e9aec0.png"
+  },
+  {
+    "revision": "65424e392cda0f379782062951987b37",
+    "url": "/DnDSite/static/media/abilities.65424e39.png"
+  },
+  {
+    "revision": "1f5cd1e927195619186c229831302d28",
+    "url": "/DnDSite/static/media/critRole.1f5cd1e9.jpg"
+  },
+  {
+    "revision": "a1806c0ecb04a6cd7222a9aafb5542d0",
+    "url": "/DnDSite/static/media/dndHome.a1806c0e.jpg"
+  },
+  {
+    "revision": "eae369639b30d1ca527483225fd62c95",
+    "url": "/DnDSite/static/media/dndLink.eae36963.jpg"
+  },
+  {
+    "revision": "85adf435b46f1af90289b01c7431352d",
+    "url": "/DnDSite/static/media/monster.85adf435.jpeg"
+  },
+  {
+    "revision": "b0ef4b2841986f97322bcf6e6bf25e93",
+    "url": "/DnDSite/static/media/spells.b0ef4b28.png"
+  },
+  {
+    "revision": "a90e47c051e8193431cd49fe9a448508",
+    "url": "/DnDSite/static/media/titleIcon.a90e47c0.png"
+  }
+]);
